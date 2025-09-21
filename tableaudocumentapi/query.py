813a7b94ba35247ad6304fc8c2d7a4cb8a060076
jbisal <justin.s.bisal@gmail.com>
2025-09-20 19:16:28 -0600

    Cleaned up code

--- tableaudocumentapi/query.py
+++ tableaudocumentapi/query.py
@@ -102,20 +102,21 @@ class Query(object):
     
     
     def get_workbook_fields(self):
-        """Get all Fields in a workbook and their attributes as a list of dictionaries"""
-        import pdb; pdb.set_trace()
+        """Get all non-parameter Fields in a workbook and their attributes as a list of dictionaries"""
         field_attributes = [
             'alias', 'aliases', 'calculation', 'caption', 'datatype', 'default_aggregation',
             'description', 'hidden', 'id', 'is_nominal', 'is_ordinal','is_quantitative', 
             'name', 'param_domain_type', 'role', 'table', 'type','value','worksheets']
         workbook_fields = []
         for datasource in self._workbook.datasources:
-             fields = datasource.fields
-             for _ in fields:
-                field_dict = {}
-                field_dict['datasource'] = datasource.name
-                field_dict['field_key'] = _
-                for field_attribute in field_attributes:
-                    field_dict[field_attribute] = getattr(fields[_],field_attribute)
-                workbook_fields.append(field_dict)
+            if datasource.name != "Parameters":
+                fields = datasource.fields
+                for key in fields:
+                    if key.startswith('[') and key.endswith(']'):
+                        field_dict = {}
+                        field_dict['datasource'] = datasource.name
+                        field_dict['field_key'] = key
+                        for field_attribute in field_attributes:
+                            field_dict[field_attribute] = getattr(fields[key],field_attribute)
+                        workbook_fields.append(field_dict)
         return workbook_fields
